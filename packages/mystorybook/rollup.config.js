882239a0958496611b550c56ce85923981aa729f
Komal Singh <ext_ksingh@falabella.cl>
2022-04-20 12:26:20 +0530

    Eslint setup

--- packages/mystorybook/rollup.config.js
+++ packages/mystorybook/rollup.config.js
@@ -1,33 +1,33 @@
-import { babel } from "@rollup/plugin-babel";
-import external from "rollup-plugin-peer-deps-external";
-import resolve from "@rollup/plugin-node-resolve";
-import scss from "rollup-plugin-scss";
-import typescript from "@rollup/plugin-typescript";
-import { terser } from "rollup-plugin-terser";
+import { babel } from '@rollup/plugin-babel';
+import external from 'rollup-plugin-peer-deps-external';
+import resolve from '@rollup/plugin-node-resolve';
+import scss from 'rollup-plugin-scss';
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
 
 export default [
   {
-    input: "./src/index.ts",
+    input: './src/index.ts',
     output: [
       {
-        file: "dist/index.js",
-        format: "cjs",
+        file: 'dist/index.js',
+        format: 'cjs',
       },
       {
-        file: "dist/index.es.js",
-        format: "es",
-        exports: "named",
+        file: 'dist/index.es.js',
+        format: 'es',
+        exports: 'named',
       },
     ],
     plugins: [
       scss({
         output: true,
         failOnError: true,
-        outputStyle: "compressed",
+        outputStyle: 'compressed',
       }),
       babel({
-        exclude: "node_modules/**",
-        presets: ["@babel/preset-react"],
+        exclude: 'node_modules/**',
+        presets: ['@babel/preset-react'],
       }),
       external(),
       resolve(),
